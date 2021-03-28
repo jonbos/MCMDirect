b40ef500988e68de305f664edd1ee1f73ffad5f5
Jon Bos <jbos@mumosystems.com>
2021-03-28 13:28:21 -0500

    context setup

--- MCMDirect/Models/DataLayer/MCMContext.cs
+++ MCMDirect/Models/DataLayer/MCMContext.cs
@@ -5,6 +5,10 @@ namespace MCMDirect.Areas.Store.Models {
     public class MCMContext : DbContext {
         public DbSet<Product> Products { get; set; }
 
+        public MCMContext(DbContextOptions options) : base(options)
+        {
+        }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
