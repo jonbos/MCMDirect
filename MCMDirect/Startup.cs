f4f37113f33262d892d88f37f97d185dadff5f45
Jon Bos <jbos@mumosystems.com>
2021-03-28 13:23:46 -0500

    service setup

--- MCMDirect/Startup.cs
+++ MCMDirect/Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MCMDirect.Areas.Store.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -21,7 +23,9 @@ namespace MCMDirect {
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllersWithViews().AddNewtonsoftJson();
+            services.AddDbContext<MCMContext>(options =>
+                options.UseSqlite(Configuration.GetConnectionString("MCMContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
