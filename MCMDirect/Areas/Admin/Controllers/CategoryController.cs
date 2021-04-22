03fd523ce334945021ad0d9edc0f29550ecfac02
Jon Bos <jbos@mumosystems.com>
2021-04-22 10:12:05 -0500

    --wip-- [skip ci]

--- MCMDirect/Areas/Admin/Controllers/CategoryController.cs
+++ MCMDirect/Areas/Admin/Controllers/CategoryController.cs
@@ -1,11 +1,24 @@
+using System.Linq;
+using MCMDirect.Areas.Store.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace MCMDirect.Areas.Admin.Controllers {
     [Area("Admin")]
+    [Authorize(Roles = "Admin")]
     public class CategoryController : Controller {
+        private MCMContext _context;
+
+        public CategoryController(MCMContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var data = _context.Category.Include(category => category.Products).ToList();
+            return View(data);
         }
     }
 }
