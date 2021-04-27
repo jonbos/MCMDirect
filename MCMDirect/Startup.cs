1f4c60af59a4d1ddad96df35c4593b4c51e60285
Jon Bos <jbos@mumosystems.com>
2021-04-27 11:06:41 -0500

    Use session in startup

--- MCMDirect/Startup.cs
+++ MCMDirect/Startup.cs
@@ -57,13 +57,14 @@ namespace MCMDirect {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
             var cultureInfo = new CultureInfo("en-US");
             cultureInfo.NumberFormat.CurrencySymbol = "$";
 
             CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
             CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
 
-
+            app.UseSession();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
